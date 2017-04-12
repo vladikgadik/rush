2f5d3519ff506ce673fed41e4cd6395bcd1a36fe
vladikgadik <vlad81@i.ua>
2017-04-12 21:32:49 +0200

    three

--- src/com/javarush/task/task18/task1812/QuestionFileOutputStream.java
+++ src/com/javarush/task/task18/task1812/QuestionFileOutputStream.java
@@ -7,6 +7,39 @@ import java.io.*;
 */
 
 public class QuestionFileOutputStream implements AmigoOutputStream {
+   private AmigoOutputStream amigoOutputStream;
+    public QuestionFileOutputStream(AmigoOutputStream amigoOutputStream){
+       this.amigoOutputStream = amigoOutputStream;
+    }
 
+    @Override
+    public void flush() throws IOException {
+        amigoOutputStream.flush();
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        amigoOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        amigoOutputStream.write(b);
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) throws IOException {
+        amigoOutputStream.write(b,off,len);
+    }
+
+    @Override
+    public void close() throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Вы действительно хотите закрыть поток? Д/Н");
+            Character str = reader.readLine().charAt(0);
+            if (str == 'Д'){
+                amigoOutputStream.close();
+            }
+    }
 }
 
