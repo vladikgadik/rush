cb277b9dbabca8a414b3ebef326876ced175c6f6
vladikgadik <vlad81@i.ua>
2017-04-16 00:10:50 +0200

    15.04_14

--- src/com/javarush/task/task18/task1821/Solution.java
+++ src/com/javarush/task/task18/task1821/Solution.java
@@ -4,8 +4,31 @@ package com.javarush.task.task18.task1821;
 Встречаемость символов
 */
 
-public class Solution {
-    public static void main(String[] args) {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Map;
+import java.util.TreeMap;
 
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        FileInputStream inputstream = new FileInputStream(args[0]);
+        Map<Integer,Integer> map = new TreeMap<>();
+        while (inputstream.available()>0){
+            int value = inputstream.read();
+          int a;
+                if (map.containsKey(value)) {
+                    int counter = map.get(value) + 1;
+                    map.put(value, counter);
+                } else {
+                    map.put(value, 1);
+                }
+
+        }
+        for (Map.Entry <Integer,Integer> pair : map.entrySet()) {
+            int ch =pair.getKey();
+            char c = (char) ch;
+            System.out.println( c+" "+pair.getValue());
+        }
+        inputstream.close();
     }
 }
