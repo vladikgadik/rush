de4ab3a97787b169331459d0eefaac58122a90aa
vladikgadik <vlad81@i.ua>
2017-04-30 13:52:07 +0200

    30.04_4

--- src/com/javarush/task/task19/task1910/Solution.java
+++ src/com/javarush/task/task19/task1910/Solution.java
@@ -4,7 +4,23 @@ package com.javarush.task.task19.task1910;
 Пунктуация
 */
 
+import java.io.*;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String oneName = reader.readLine();
+        String twoName = reader.readLine();
+        reader.close();
+        BufferedReader fileRead = new BufferedReader(new FileReader(oneName));
+        StringBuilder str = new StringBuilder();
+        while (fileRead.ready()){
+            str.append(fileRead.readLine()).append("\n");
+        }
+        fileRead.close();
+        String result = str.toString().replaceAll("\\W","");
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(twoName));
+        bufferedWriter.write(result.toString());
+        bufferedWriter.close();
     }
 }
