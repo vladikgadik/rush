1de1e1e53b0bfee0de4916f1240057e0dc1497e3
vladikgadik <vlad81@i.ua>
2017-05-03 17:52:27 +0200

    03.05_2

--- src/com/javarush/task/task19/task1917/FileConsoleWriter.java
+++ src/com/javarush/task/task19/task1917/FileConsoleWriter.java
@@ -4,8 +4,62 @@ package com.javarush.task.task19.task1917;
 Свой FileWriter
 */
 
+import java.io.File;
+import java.io.FileDescriptor;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class FileConsoleWriter {
-    public static void main(String[] args) {
+    private FileWriter fileWriter;
+
+    public FileConsoleWriter(String name) throws IOException {
+        fileWriter = new FileWriter(name);
+    }
+    public FileConsoleWriter(File file) throws IOException{
+        fileWriter = new FileWriter(file);
+    }
+    public FileConsoleWriter(String name, boolean append) throws IOException{
+        fileWriter = new FileWriter(name, append);
+    }
+    public FileConsoleWriter(File file, boolean append) throws IOException{
+        fileWriter = new FileWriter(file, append);
+    }
+    public FileConsoleWriter(FileDescriptor fd){
+        fileWriter = new FileWriter(fd);
+    }
+
+
+    public void write(int c) throws IOException {
+        fileWriter.write(c);
+        System.out.println(c);
+    }
+
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        fileWriter.write(cbuf, off, len);
+        System.out.println(String.valueOf(cbuf).substring(off,off+len));
+    }
+
+    public void write(String str, int off, int len) throws IOException {
+        fileWriter.write(str, off, len);
+        System.out.println(str.substring(off,off + len));
+    }
+
+    public void write(char[] cbuf) throws IOException {
+        fileWriter.write(cbuf);
+        System.out.println(String.valueOf(cbuf));
+    }
+
+    public void write(String str) throws IOException {
+        fileWriter.write(str);
+        System.out.println(str);
+    }
+
+    public void close() throws IOException{
+        fileWriter.close();
+    }
+
+
+    public static void main(String[] args) throws IOException {
 
     }
 
