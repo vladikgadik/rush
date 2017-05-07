ccde91bc273fe321049a689e0daa8245748c3ba1
vladikgadik <vlad81@i.ua>
2017-05-07 17:23:10 +0200

    07.05_2

--- src/com/javarush/task/task19/task1923/Solution.java
+++ src/com/javarush/task/task19/task1923/Solution.java
@@ -4,7 +4,29 @@ package com.javarush.task.task19.task1923;
 Слова с цифрами
 */
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader fileRead = new BufferedReader(new FileReader(args[0]));
+        List<String> listData = new ArrayList<>();
+        while (fileRead.ready()){
+            listData.add(fileRead.readLine());
+        }
+        fileRead.close();
+        StringBuilder result = new StringBuilder();
+        for (int i = 0;i<listData.size();i++){
+            String[] lineData = listData.get(i).split("\\s+") ;
+                for (int j=0;j<lineData.length;j++){
+                    if (lineData[j].matches("^\\S*\\d+\\S*$")){
+                        result.append(lineData[j]).append(" ");
+                    }
+                }
+        }
+        BufferedWriter writeFile = new BufferedWriter(new FileWriter(args[1]));
+        writeFile.write(result.toString());
+        writeFile.close();
     }
 }
