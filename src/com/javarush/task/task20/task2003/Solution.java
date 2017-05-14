bc07818b48b886fdd3ade1ddc3ac8c9b0da07615
vladikgadik <vlad81@i.ua>
2017-05-14 17:09:40 +0200

    14.05_2

--- src/com/javarush/task/task20/task2003/Solution.java
+++ src/com/javarush/task/task20/task2003/Solution.java
@@ -1,9 +1,9 @@
 package com.javarush.task.task20.task2003;
 
-import java.io.InputStream;
-import java.io.OutputStream;
+import java.io.*;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 
 /* 
 Знакомство с properties
@@ -11,19 +11,40 @@ import java.util.Map;
 public class Solution {
     public static Map<String, String> properties = new HashMap<>();
 
-    public void fillInPropertiesMap() {
+    public void fillInPropertiesMap() throws Exception {
         //implement this method - реализуйте этот метод
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String fileName = reader.readLine();
+        reader.close();
+        InputStream inputStream = new FileInputStream(fileName);
+        load(inputStream);
     }
 
     public void save(OutputStream outputStream) throws Exception {
         //implement this method - реализуйте этот метод
+        Properties props = new Properties();
+        for (Map.Entry<String, String> pairs : properties.entrySet()) {
+            props.put(pairs.getKey(), pairs.getValue());
+        }
+        props.store(outputStream, "");
     }
 
     public void load(InputStream inputStream) throws Exception {
         //implement this method - реализуйте этот метод
+        Properties props = new Properties();
+        props.load(inputStream);
+
+        for (Map.Entry<Object, Object> pairs : props.entrySet()) {
+            properties.put((String)pairs.getKey(), (String)pairs.getValue());
+        }
     }
 
-    public static void main(String[] args) {
-
+    public static void main(String[] args) throws Exception {
+        Solution solution = new Solution();
+        solution.fillInPropertiesMap();
+        for (Map.Entry<String,String> map:properties.entrySet()){
+            System.out.println(map.getKey()+"..."+map.getValue());
+        }
+        solution.save(new FileOutputStream("D:/2.txt"));
     }
 }
